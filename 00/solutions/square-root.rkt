b9e1077ce28c285f0a9b195249f7cc5bb14dcb41
Parashkev Katerski <pkaterski@Parashkevs-MacBook-Pro.local>
2019-11-02 22:17:21 +0200

    λ

--- 00/solutions/square-root.rkt
+++ 00/solutions/square-root.rkt
@@ -2,4 +2,18 @@
 
 (provide my-sqrt)
 
-(define (my-sqrt x) void)
+
+(define (my-sqrt x)
+  (define (newtonche i)
+    (if (<
+         (- (* i i) x)
+         0.0001) ; tochnostta
+        i
+        (newtonche
+         (-
+          i
+          (/
+           (- (* i i) x)
+           (* 2 i))))))
+  (newtonche (/ x 2)))
+
