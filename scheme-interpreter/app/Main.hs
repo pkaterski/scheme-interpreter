180eaf3a5d15a92c1869974c0c4cc5bf7236f6d9
Parashkev Katerski <pkaterski@gmail.com>
2020-03-27 14:21:22 +0200

    add dynamic autocomplete

--- scheme-interpreter/app/Main.hs
+++ scheme-interpreter/app/Main.hs
@@ -13,33 +13,33 @@ searchFunc defs str =
 defsToNames :: State -> [String]
 defsToNames = map $ \(Definition name _) -> name
 
-mySettings :: Settings IO
-mySettings = Settings 
+mySettings :: State -> Settings IO
+mySettings defs = Settings 
     { historyFile = Just "myhist"
     , complete = completeWord Nothing " \t" $ \str -> do
-        -- defs <- get
-        pure $ searchFunc defaultDefs str
+        pure $ searchFunc defs str
     , autoAddHistory = True
     }
+
+
+loop :: State -> IO ()
+loop defs = do
+    minput <- runInputT (mySettings defs) $ getInputLine "> "
+    case minput of
+        Nothing -> return ()
+        Just ":quit" -> return ()
+        Just line -> do
+            let (res, defs') = runExpression line defs
+            putStr res
+            loop defs'
 
 main :: IO ()
 main = do
     lib <- interpretFile "lib/prelude.pisp"
     case lib of
         Right (defs, _)-> do
-            runInputT mySettings $ loop $ defs ++ defaultDefs
+            loop $ defs ++ defaultDefs
         Left err -> putStrLn $ "err: the Prelude couldn't load: " ++ err
-    
-    where
-    loop :: State -> InputT IO ()
-    loop defs = do
-        minput <- getInputLine "> "
-        -- defs <- get
-        case minput of
-            Nothing -> return ()
-            Just ":quit" -> return ()
-            Just line -> do
-                let (res, defs') = runExpression line defs
-                outputStr res
-                loop defs'
+
+
 
